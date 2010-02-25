e5326971e59f209fc470ea4b08b547afcfacf68c
TheX <seh.css@web.de>
2010-02-25 18:05:49 +0100

    r1 Grundgerüst hinzugefügt

---
+++ HomeworkManager/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden 
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die mit einer Assembly verknüpft sind.
+
+' Die Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("HomeworkManager")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("HomeworkManager")> 
+<Assembly: AssemblyProduct("HomeworkManager")> 
+<Assembly: AssemblyCopyright("Copyright © HomeworkManager 2010")> 
+<Assembly: AssemblyTrademark("HomeworkManager")> 
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+<Assembly: Guid("a42d3258-1bc6-43b9-9182-2412619ff17e")> 
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion 
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+' übernehmen, indem Sie "*" eingeben:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
